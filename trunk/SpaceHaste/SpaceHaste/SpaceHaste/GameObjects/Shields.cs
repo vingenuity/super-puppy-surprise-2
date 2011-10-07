8e64ef0d595343cda232ad08c4fea17554d0d5dc
trogs@rocketmail.com <trogs@rocketmail.com@d46a54b8-87dd-8360-0e66-63ad6548c8d3>
2011-10-07 15:46:36 +0000

    Created a basic ship class and shields class

---
+++ trunk/SpaceHaste/SpaceHaste/SpaceHaste/GameObjects/Shields.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SpaceHaste.GameObjects
+{
+    public class Shields
+    {
+        double direction;
+        double strength;
+
+        public Shields() { }
+
+    }
+}
---
+++ trunk/SpaceHaste/SpaceHaste/SpaceHaste/GameObjects/Ship.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SpaceHaste.GameObjects
+{
+    public class Ship
+    {
+        int ammo;
+        int laserAttack;
+        int hull;
+        double regenEff;
+        double engineEff;
+        double laserEff;
+        double shieldsEff;
+        double energy;
+        //shield shield;        **Need shield type
+
+        public Ship() { }
+
+        static void Laser() { 
+            //Laser attack against enemy
+        }
+
+        static void Missile() { 
+            //Missile attack against enemy
+        }
+
+        static void isHitMissile() { 
+            //ship is hit by a missile attack
+        }
+
+        static void isHitLaser() { 
+            //ship is hit by a laser attack
+        }
+
+
+        
+
+    }
+}

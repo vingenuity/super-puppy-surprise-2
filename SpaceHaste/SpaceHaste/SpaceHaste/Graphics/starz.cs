a9b7739cf98cb39b677b62dab23387abe41ff09f
trogs@rocketmail.com <trogs@rocketmail.com@d46a54b8-87dd-8360-0e66-63ad6548c8d3>
2011-10-10 16:03:37 +0000

    added starz class to hold billboard data from maya to be rendered into the game.

---
+++ SpaceHaste/SpaceHaste/SpaceHaste/Graphics/starz.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+//This class holds position and any other information necessary for rendering out star and other visual asset bilboards in 3d space
+namespace SpaceHaste.Graphics
+{
+    public class starz
+    {
+        List<Vector3> StarPoints;       //Holds star positions  StarPoints = new List<Vector
+
+        public starz() {
+            StarPoints = new List<Vector3>();
+
+        }
+        public void AddStars()
+        {
+            //StarPoints.Add(new Vector3(4,4,4));
+        }
+    }
+}

4dc2e77784f556221cc1390bd4ecf71d9c6e6ec1
howlingblue@gmail.com <howlingblue@gmail.com@d46a54b8-87dd-8360-0e66-63ad6548c8d3>
2011-10-15 21:22:24 +0000

    Updated Ship with some damage functions. Targeting not yet implemented. Updated Shield class, but currently using simpler one in Ship. Updated project to include GridCube(wasn't in last commit for some reason.)

--- SpaceHaste/SpaceHaste/SpaceHaste/GameObjects/Shields.cs
+++ SpaceHaste/SpaceHaste/SpaceHaste/GameObjects/Shields.cs
@@ -7,10 +7,32 @@ namespace SpaceHaste.GameObjects
 {
     public class Shields
     {
-        double direction;
-        double strength;
+        //Directions of the shields(front, back, left, right, up, down, non-nautically)
+        public enum dir{bow=1, stern=2, port=4, starboard=8, top=16, bottom=32}
+        private int dirs;
+        private int max;
+        private int power;
 
-        public Shields() { }
+        public Shields(int maxPower) 
+        {
+            dirs = 63;
+            max = maxPower;
+            power = 0;
+        }
 
+        public void directToAll()
+        {
+            dirs = 63;
+        }
+
+        public void recharge(int amount)
+        {
+            power += amount;
+        }
+
+        public void directTo(dir direction)
+        {
+            dirs = (int)direction;
+        }
     }
 }
--- SpaceHaste/SpaceHaste/SpaceHaste/GameObjects/Ship.cs
+++ SpaceHaste/SpaceHaste/SpaceHaste/GameObjects/Ship.cs
@@ -9,51 +9,81 @@ namespace SpaceHaste.GameObjects
 {
     public class Ship : GameObject
     {
-        double[] hull = new double[2];          //health arr 0=current 1=max
-        double[] energy = new double[2];        //energy arr 0=current 1=max
-        int numMiss;                            //Number of missiles ship has left                     
-        double[] dmg = new double[2];           //Damage done by weapons 0= Laser, 1= Missile
-        double[] efficiency= new double[3];     //contains [moveEff,laserEff,shieldsEff] efficiencies track energy usage for specified action
-        double eRegen;                          //Amount energy regens per tern                       
-        //shield shield;        **Need shield type
+        //Health
+        int[] hull = new int[2];          //health  -- 0 = current, 1 = max
+        int[] shield = new int[2];        //shields -- 0 = current, 1 = max
+
+        //Energy
+        int[] energy = new int[2];        //energy  -- 0 = current, 1 = max
+        int enRegen = 20;                    //Amount energy regenerates per turn 
+        double[] efficiency = new double[3];    //contains [moveEff,laserEff,shieldsEff] efficiencies track energy usage for specified action                        
+
+        //Weapons
+        int[] dmg = new int[2];           //Damage done by weapons 0 = Laser, 1 = Missile
+        int numMiss;                            //Number of missiles ship has left              
 
         public Ship()
-            : this(100, 100, 1, 20, 10, new double[] {.5, .5, .5}, 50){ }
+            : this(100, 100, 100, 1, 20, 10, new double[] {.5, .5, .5}){ }
 
-        public Ship(double maxHull,double maxEnergy, int numMissiles, double lsrDmg, double missDmg, double[] eff,double regen) {
-            //Hull and energy only initialize max set health and energy to max
+        public Ship(int maxHull, int maxShield, int maxEnergy, int numMissiles, int lsrDmg, int missDmg, double[] eff) 
+        {
+            //Fill hull and shields to max.
             hull[0] = hull[1] = maxHull;
+            shield[0] = shield[1] = maxShield;
 
-            energy[0] = energy[1] = maxEnergy;
+            //Start energy at zero. We will recharge energy starting on turn 1.
+            energy[0] = 0;
+            energy[1] = maxEnergy;
 
+            //Set up our weapons
             numMiss = numMissiles;
-
             dmg[0] = lsrDmg;
             dmg[1] = missDmg;
 
+            //Set up our rendering options.
             Model = GraphicsManager.Content.Load<Model>("Ship");
             GraphicsManager.GraphicsGameObjects.Add(this);
-            Scale = .5f;
+            Scale = .25f;
         }
 
-        public void Laser() { 
-            //Laser attack against enemy
+        public void fireLaser(Ship ship)
+        {
+            ship.isHit(dmg[0]);
         }
 
-        public void Missile() { 
-            //Missile attack against enemy
+        public void fireMissile(Ship ship) 
+        {
+            ship.isHit(dmg[1]);
         }
 
-        public void isHitMissile() { 
-            //ship is hit by a missile attack
+        public void isHit(int damage)
+        {
+            //If our shield can absorb it all, deal damage and return.
+            if (shield[0] > damage)
+            {
+                shield[0] -= damage;
+                return;
+            }
+            //Otherwise, take all of the damage and proceed to hull.
+            else
+            {
+                damage -= shield[0];
+                shield[0] = 0;
+            }
+
+            //If our hull can take it, deal remaining damage and exit
+            if (hull[0] > damage)
+            {
+                hull[0] -= damage;
+                return;
+            }
+            //Otherwise, we're dead. Get rid of us.
+            else
+                Unload();
         }
 
-        public void isHitLaser() { 
-            //ship is hit by a laser attack
+        public void Unload()
+        {
         }
-
-        
-        
-
     }
 }

f4672955c576284f438920e54c9073ce2dc08d91
mthomasleary@gmail.com <mthomasleary@gmail.com@d46a54b8-87dd-8360-0e66-63ad6548c8d3>
2011-10-17 15:58:06 +0000

    broken

---
+++ SpaceHaste/SpaceHaste/SpaceHaste/Primitives/QuadManager.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using System.Collections.Generic;
+
+namespace SpaceHaste.Primitives
+{
+    public class QuadManager : DrawableGameComponent
+    {
+        public static List<Quad> Quads;
+        GraphicsDeviceManager graphics;
+        public QuadManager(Game game, GraphicsDeviceManager graphics)
+            : base(game)
+        {
+            Quads = new List<Quad>();
+            this.graphics = graphics;
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            for (int i = 0; i < Quads.Count; i++)
+                Quads[i].DrawLine(graphics);
+            base.Draw(gameTime);
+        }
+        public static void AddLine(Quad quad)
+        {
+            Quads.Add(quad);
+        }
+        public static void Clear()
+        {
+            Quads = new List<Quad>();
+        }
+        public static void RemoveLine(Quad quad)
+        {
+            Quads.Remove(quad);
+        }
+    }
+}

4a7a2894c165277b4899a64010180095ee1b9392
howlingblue@gmail.com <howlingblue@gmail.com@d46a54b8-87dd-8360-0e66-63ad6548c8d3>
2011-10-28 14:55:23 +0000

    Added Team structure.

--- trunk/SpaceHaste/SpaceHaste/SpaceHaste/GameObjects/GameObject.cs
+++ trunk/SpaceHaste/SpaceHaste/SpaceHaste/GameObjects/GameObject.cs
@@ -15,29 +15,31 @@ namespace SpaceHaste.GameObjects
 {
     public class GameObject
     {
-        //Our info for engine
-        public Vector3 GridPosition;    //Our notion of position within game grid
+        //Model/Render Information
+        public float Scale;
+        public Model Model;             //Holds model info and effect information
+        public Matrix World;            //Render position of model
         public Vector3 DrawPosition;
-        public Vector3 Velocity;        //Might be abstracted into ship, Speed of ship movement to new square
-        public Vector3 Direction;       //Unit Vector of the direction the ship is facing
+        //Position Information
         public BoundingSphere boundingSphere;
-        public Boolean Passable;
-        public int Team;
-        public int LaserRange;
+        public Vector3 Direction;       //Unit Vector of the direction the ship is facing
+        public GridCube GridLocation;   //Cube of map where object is.
+        public Vector3 GridPosition;    //Our notion of position within game grid
+        //Energy Information
         public double Energy;
         public double MovementEnergyCost = 20;
         public double AttackEnergyCost = 30;
         public double EnergyEfficiency = 4;
-        public GridCube GridLocation;
+        //Ship Information(For HUD)
         public string Name;
         public double Health;
         public double maxHealth;
+        public enum Team { Player = 0, Enemy = 1 }
+        public Team team;
+        //Player Selection Information
+        public int LaserRange;
         public int MovementRange;
         public double waitTime;
-        //Model/Render Info
-        public float Scale;
-        public Model Model;             //Holds model info and effect information
-        public Matrix World;            //Render position of model
 
         //Constructor
         public GameObject(Vector3 position)
@@ -45,16 +47,16 @@ namespace SpaceHaste.GameObjects
             Energy = 100;
             Load();
             GridPosition = position;
+            team = Team.Player;
             MovementRange = (int) (Energy / MovementEnergyCost);
-	    LaserRange = 6;
+	        LaserRange = 6;
         }
+        //Creation and Deletion
         public virtual void Load()
         {
             GraphicsManager.GraphicsGameObjects.Add(this);
             GameMechanicsManager.GameObjectList.Add(this);
         }
-
-
         public virtual void Unload()
         {
             GridLocation.RemoveObject(this);
@@ -63,24 +65,29 @@ namespace SpaceHaste.GameObjects
             GameMechanicsManager.GameObjectList.Remove(this);
         }
 
-        public int LaserDamage { 
-            get; 
-            set; 
+        //Object Actions
+        public void AddEnergy(double energy)
+        {
+            Energy += energy;
+            waitTime -= energy;
+        }
+        public int LaserDamage
+        {
+            get;
+            set;
         }
 
+        //Team Operations
+        public Team getTeam() { return team; }
+        public void setTeam(Team side) { team = side; }
+
+        //Operators(for sorting)
         public static bool operator >(GameObject go1, GameObject go2)
         {
             if (go1.Energy + go1.waitTime > go2.Energy + go2.waitTime)
                 return true;
             else return false;
         }
-
-        public void AddEnergy(double energy)
-        {
-            Energy += energy;
-            waitTime -= energy;
-        }
-
         public static bool operator <(GameObject go1, GameObject go2)
         {
             if (go1.Energy + go1.waitTime < go2.Energy + go2.waitTime)

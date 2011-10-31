9ef129fc615f0555dd163b165f3ecd87b8da9f81
mthomasleary@gmail.com <mthomasleary@gmail.com@d46a54b8-87dd-8360-0e66-63ad6548c8d3>
2011-10-31 14:47:34 +0000

    Changing camera angle changes the grids. (in MapManager.cs)

--- trunk/SpaceHaste/SpaceHaste/SpaceHaste/Maps/Map.cs
+++ trunk/SpaceHaste/SpaceHaste/SpaceHaste/Maps/Map.cs
@@ -12,6 +12,9 @@ namespace SpaceHaste.Maps
     {
         protected GridCube[, ,] MapGridCubes;
         protected GridCube[, ,] BottomMap;
+        protected Line[,] XYMatrix;
+        protected Line[,] XZMatrix;
+        protected Line[,] YZMatrix;
         public List<GameObject> MapObjects;
         public int Size;
 
@@ -21,6 +24,10 @@ namespace SpaceHaste.Maps
         {
             this.Size = Size;
             MapObjects = new List<GameObject>();
+
+            XYMatrix = new Line[2, Size + 1];
+            XZMatrix = new Line[2, Size + 1];
+            YZMatrix = new Line[2, Size + 1];
 
             InitMapGridCubes();
             InitMapGameObjects();
@@ -90,15 +97,15 @@ namespace SpaceHaste.Maps
         /// axis-bound distance check eliminates game objects out of range
         /// a ray test checks if objects are blocked
         /// </summary>
-        /// <param name="gc"></param>
-        /// <param name="range"></param>
-        /// <returns></returns>
-        public Boolean IsObjectInRange(GameObject go, int range, GameObject target)
+        /// <param name="go"> selected game object, trying to fire a laser</param>
+        /// <param name="target"> targeted object </param>
+        /// <returns> true if the target is within laser range of the go</returns>
+        public Boolean IsObjectInRange(GameObject go, GameObject target)
         {
             int d = (int)Math.Abs(go.GridPosition.X - target.GridPosition.X) +
                     (int)Math.Abs(go.GridPosition.Y - target.GridPosition.Y) +
                     (int)Math.Abs(go.GridPosition.Z - target.GridPosition.Z);
-            if (d > range)
+            if (d > go.LaserRange)
                 return false;
 
             Vector3 rayDirection = target.GridPosition - go.GridPosition;
@@ -119,6 +126,7 @@ namespace SpaceHaste.Maps
         /// <summary>
         /// Finds the number of grid squares in range of a particular grid square.
         /// This will presumably be used to find valid moves for each ship.
+        /// This function does a depth first search
         /// </summary>
         /// <param name="loc">Grid square to start the search.</param>
         /// <param name="range">Distance of squares away to search.</param>
@@ -154,6 +162,7 @@ namespace SpaceHaste.Maps
         /// <summary>
         /// Finds the number of grid squares in range of a grid square x, y, z.
         /// This extends the functionality of the above function for more general use.
+        /// This function calls GetGridSquaresInRange for the GridCube at location loc
         /// </summary>
         /// <param name="loc"> location to start the search.</param>
         /// <param name="range"> number of neighboring squares to search.</param>
@@ -198,6 +207,13 @@ namespace SpaceHaste.Maps
             }
         }
 
+        /// <summary>
+        /// 0 Version:
+        /// These functions add our side grids for each plane: XY, XZ, YZ.
+        /// Each fucntion stores the added lines to a matrix.
+        /// AddGridIsometric() calls the AddGrid for each plane.
+        /// AddGrid XYZ is never called.
+        /// </summary>
         public void AddGridXY0()
         {
             for (int i = 0; i <= Size; i++)
@@ -205,10 +221,16 @@ namespace SpaceHaste.Maps
                 float x = ((Size / 2) - i) * GridCube.GRIDSQUARELENGTH;
                 float y = Size / 2 * GridCube.GRIDSQUARELENGTH;
                 float z = -1 * Size / 2 * GridCube.GRIDSQUARELENGTH;
-                LineManager.AddLine(new Line(new Vector3(x, y , z), new Vector3(x, -y, z), 
-                                    new Color((float) (Size - i) * .1f, 0, 0)));
-                LineManager.AddLine(new Line(new Vector3(y, x, z), new Vector3(-y, x, z),
-                                    new Color((float) (Size - i) * .1f, 0, 0)));
+
+                Line line = new Line(new Vector3(x, y, z), new Vector3(x, -y, z),
+                                     new Color((float)(Size - i) * .1f, 0, 0));
+                LineManager.AddLine(line);
+                XYMatrix[0, i] = line;
+
+                Line orthongal = new Line(new Vector3(y, x, z), new Vector3(-y, x, z),
+                                          new Color((float)(Size - i) * .1f, 0, 0));
+                LineManager.AddLine(orthongal);
+                XYMatrix[1, i] = orthongal;
             }
                 
 
@@ -220,10 +242,16 @@ namespace SpaceHaste.Maps
                 float x = -1 * Size / 2 * GridCube.GRIDSQUARELENGTH;
                 float y = Size / 2 * GridCube.GRIDSQUARELENGTH;
                 float z = ((Size / 2) - i) * GridCube.GRIDSQUARELENGTH;
-                LineManager.AddLine(new Line(new Vector3(x, y, z), new Vector3(x, -y, z),
-                                    new Color(0, (float)(Size - i) * .1f, 0)));
-                LineManager.AddLine(new Line(new Vector3(x, z, y), new Vector3(x, z, -y),
-                                    new Color(0, (float)(Size - i) * .1f, 0)));
+
+                Line line = new Line(new Vector3(x, y, z), new Vector3(x, -y, z),
+                                     new Color(0, (float)(Size - i) * .1f, 0));
+                LineManager.AddLine(line);
+                YZMatrix[0, i] = line;
+
+                Line orthogonal = new Line(new Vector3(x, z, y), new Vector3(x, z, -y),
+                                           new Color(0, (float)(Size - i) * .1f, 0));
+                LineManager.AddLine(orthogonal);
+                YZMatrix[1, i] = orthogonal;
             }
 
 
@@ -235,35 +263,125 @@ namespace SpaceHaste.Maps
                 float x = ((Size / 2) - i) * GridCube.GRIDSQUARELENGTH;
                 float z = Size / 2 * GridCube.GRIDSQUARELENGTH;
                 float y = -1 * (Size / 2) * GridCube.GRIDSQUARELENGTH;
-                LineManager.AddLine(new Line(new Vector3(x, y, z), new Vector3(x, y, -z), 
-                                    new Color(0, 0, (float) (Size - i) * .1f )));
-                LineManager.AddLine(new Line(new Vector3(z, y, x), new Vector3(-z, y, x),
-                                    new Color(0, 0, (float) (Size - i) * .1f)));
-            }
-        }
-        public void AddGridXYZ()
-        {
-            for (int j = 0; j <= Size; j++)
-            {
-                float z = ((Size / 2) - j) * GridCube.GRIDSQUARELENGTH;
-                for (int i = 0; i <= Size; i++)
-                {
-                    float x = ((Size / 2) - i) * GridCube.GRIDSQUARELENGTH;
-                    float y = Size / 2 * GridCube.GRIDSQUARELENGTH;
-                    LineManager.AddLine(new Line(new Vector3(x, y, z),
-                                        new Vector3(x, -y, z)));
-                    LineManager.AddLine(new Line(new Vector3(y, x, z),
-                                       new Vector3(-y, x, z)));
-                }
 
-            }
+                Line line = new Line(new Vector3(x, y, z), new Vector3(x, y, -z), 
+                                    new Color(0, 0, (float) (Size - i) * .1f ));
+                LineManager.AddLine(line);
+                XZMatrix[0, i] = line;
 
+                Line orthogonal = new Line(new Vector3(z, y, x), new Vector3(-z, y, x),
+                                    new Color(0, 0, (float) (Size - i) * .1f));
+                LineManager.AddLine(orthogonal);
+                XZMatrix[1, i] = orthogonal;
+            }
         }
         public void AddGridIsometric()
         {
             AddGrid0YZ();
             AddGridX0Z();
             AddGridXY0();
+        }
+
+        /// <summary>
+        /// 1 Version:
+        /// These functions are analogous to the 0 versions.
+        /// Adds Grids to opposite ends of planes.
+        /// AddGridAltIsometric() calls all 1 version AddGrid functions.
+        /// </summary>
+        public void AddGridXY1()
+        {
+            for (int i = 0; i <= Size; i++)
+            {
+                float x = ((Size / 2) - i) * GridCube.GRIDSQUARELENGTH;
+                float y = Size / 2 * GridCube.GRIDSQUARELENGTH;
+                float z = Size / 2 * GridCube.GRIDSQUARELENGTH;
+
+                Line line = new Line(new Vector3(x, y, z), new Vector3(x, -y, z),
+                                     new Color((float) 1 - ((Size - i) * .1f), 0, 0));
+                LineManager.AddLine(line);
+                XYMatrix[0, i] = line;
+
+                Line orthongal = new Line(new Vector3(y, x, z), new Vector3(-y, x, z),
+                                          new Color((float)1 - ((Size - i) * .1f), 0, 0));
+                LineManager.AddLine(orthongal);
+                XYMatrix[1, i] = orthongal;
+            }
+
+
+        }
+        public void AddGrid1YZ()
+        {
+            for (int i = 0; i <= Size; i++)
+            {
+                float x = Size / 2 * GridCube.GRIDSQUARELENGTH;
+                float y = Size / 2 * GridCube.GRIDSQUARELENGTH;
+                float z = ((Size / 2) - i) * GridCube.GRIDSQUARELENGTH;
+
+                Line line = new Line(new Vector3(x, y, z), new Vector3(x, -y, z),
+                                     new Color(0, (float)1 - ((Size - i) * .1f), 0));
+                LineManager.AddLine(line);
+                YZMatrix[0, i] = line;
+
+                Line orthogonal = new Line(new Vector3(x, z, y), new Vector3(x, z, -y),
+                                           new Color(0, (float)1 - ((Size - i) * .1f), 0));
+                LineManager.AddLine(orthogonal);
+                YZMatrix[1, i] = orthogonal;
+            }
+
+
+        }
+        public void AddGridX1Z()
+        {
+            for (int i = 0; i <= Size; i++)
+            {
+                float x = ((Size / 2) - i) * GridCube.GRIDSQUARELENGTH;
+                float z = Size / 2 * GridCube.GRIDSQUARELENGTH;
+                float y = (Size / 2) * GridCube.GRIDSQUARELENGTH;
+
+                Line line = new Line(new Vector3(x, y, z), new Vector3(x, y, -z),
+                                    new Color(0, 0, (float) 1 - ((Size - i) * .1f)));
+                LineManager.AddLine(line);
+                XZMatrix[0, i] = line;
+
+                Line orthogonal = new Line(new Vector3(z, y, x), new Vector3(-z, y, x),
+                                    new Color(0, 0, (float) 1 - ((Size - i) * .1f)));
+                LineManager.AddLine(orthogonal);
+                XZMatrix[1, i] = orthogonal;
+            }
+        }
+        public void AddGridAltIsometric()
+        {
+            AddGrid1YZ();
+            AddGridX1Z();
+            AddGridXY1();
+        }
+
+        /// <summary>
+        /// helper functions to move grids
+        /// </summary>
+        public void RemoveXYMatrix()
+        {
+            for(int i = 0; i <= Size; i++)
+            {
+                LineManager.RemoveLine(XYMatrix[0, i]);
+                LineManager.RemoveLine(XYMatrix[1, i]);
+            }
+        }
+        public void RemoveXZMatrix()
+        {
+            for (int i = 0; i <= Size; i++)
+            {
+                LineManager.RemoveLine(XZMatrix[0, i]);
+                LineManager.RemoveLine(XZMatrix[1, i]);
+            }
+        }
+        public void RemoveYZMatrix()
+        {
+            for (int i = 0; i <= Size; i++)
+            {
+                LineManager.RemoveLine(YZMatrix[0, i]);
+                LineManager.RemoveLine(YZMatrix[1, i]);
+            }
         }
 
         public GridCube GetCubeAt(Vector3 loc)
@@ -294,5 +412,6 @@ namespace SpaceHaste.Maps
         }
     }
 }
+
 
 
--- trunk/SpaceHaste/SpaceHaste/SpaceHaste/Maps/MapManager.cs
+++ trunk/SpaceHaste/SpaceHaste/SpaceHaste/Maps/MapManager.cs
@@ -14,5 +14,46 @@ namespace SpaceHaste.Maps
         {
             Map = new Map1();
         }
+        public override void Update(GameTime gameTime)
+        {
+            if (Controls.ControlManager.camera.getVerticalAngle() > 1.54)
+            {
+                double a = Controls.ControlManager.camera.getVerticalAngle();
+                Map.RemoveXZMatrix();
+                Map.AddGridX1Z();
+            } 
+            else 
+            {
+                double a = Controls.ControlManager.camera.getVerticalAngle();
+                Map.RemoveXZMatrix();
+                Map.AddGridX0Z();
+            }
+
+            if (Controls.ControlManager.camera.getHorizontalAngle() > 1.57
+             && Controls.ControlManager.camera.getHorizontalAngle() < 1.57 + Math.PI)
+            {
+                Map.RemoveXYMatrix();
+                Map.AddGridXY1();
+            }
+            else 
+            {
+                Map.RemoveXYMatrix();
+                Map.AddGridXY0();
+            }
+
+            if (Controls.ControlManager.camera.getHorizontalAngle() > 0 
+             && Controls.ControlManager.camera.getHorizontalAngle() < Math.PI)
+            {
+                Map.RemoveYZMatrix();
+                Map.AddGrid0YZ();
+            }
+            else
+            {
+                Map.RemoveYZMatrix();
+                Map.AddGrid1YZ();
+            }
+
+            base.Update(gameTime);
+        }
     }
 }

a69c1fa458861ecdc6f20cd2962191870169711b
motasheron@hotmail.com <motasheron@hotmail.com@d46a54b8-87dd-8360-0e66-63ad6548c8d3>
2011-11-01 02:00:51 +0000

    Menus in! Win Conditions in! Energy Recovering after each turn = 80% done!

---
+++ trunk/SpaceHaste/SpaceHaste/SpaceHaste/Screens/Credits.cs
@@ -0,0 +1,134 @@
+﻿using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using SuperPuppySurprise;
+using Microsoft.Xna.Framework.Input;
+namespace GameStateManagement
+    {
+        /// <summary>
+        /// This screen implements the actual game logic. It is just a
+        /// placeholder to get the idea across: you'll probably want to
+        /// put some more interesting gameplay in here!
+        /// </summary>
+        class CreditsScreen : GameScreen
+        {
+           
+            #region Fields
+
+
+            SpriteFont gameFont;
+
+            double currentTime;
+            int textNumber;
+
+            float pauseAlpha;
+
+            #endregion
+
+            #region Initialization
+            public static bool loaded = false;
+            double time = .5;
+            /// <summary>
+            /// Constructor.
+            /// </summary>
+            public CreditsScreen()
+            {
+            }
+            #endregion
+            Texture2D text;
+            /// <summary>
+            /// Load graphics content for the game.
+            /// </summary>
+            public override void LoadContent()
+            {
+
+             
+
+                text = Game1.game.Content.Load<Texture2D>("Credits");
+                Game1.spriteBatch = new SpriteBatch(Game1.game.GraphicsDevice);
+            }
+
+
+            /// <summary>
+            /// Unload graphics content used by the game.
+            /// </summary>
+            public override void UnloadContent()
+            {
+               
+            }
+
+            #region Update and Draw
+
+            /// <summary>
+            /// Updates the state of the game. This method checks the GameScreen.IsActive
+            /// property, so the game will stop updating when the pause menu is active,
+            /// or if you tab away to a different application.
+            /// </summary>
+            public override void Update(GameTime gameTime, bool otherScreenHasFocus,
+                                                           bool coveredByOtherScreen)
+            {
+                base.Update(gameTime, otherScreenHasFocus, false);
+
+                time -= gameTime.ElapsedGameTime.TotalSeconds;
+
+                keyboardState = Keyboard.GetState();
+                gamePadState = GamePad.GetState(PlayerIndex.One);
+
+                //
+                if (time < 0)
+                {
+                    if (keyboardState.IsKeyDown(Keys.Enter) || keyboardState.IsKeyDown(Keys.Space) || gamePadState.IsButtonDown(Buttons.Start) || gamePadState.IsButtonDown(Buttons.A))
+                        this.ExitScreen();
+                }
+
+            }
+            KeyboardState keyboardState; GamePadState gamePadState;
+
+            /// <summary>
+            /// Lets the game respond to player input. Unlike the Update method,
+            /// this will only be called when the gameplay screen is active.
+            /// </summary>
+            public override void HandleInput(InputState input)
+            {
+
+               
+
+                // Look up inputs for the active player profile.
+                int playerIndex = (int)ControllingPlayer.Value;
+
+                keyboardState = input.CurrentKeyboardStates[playerIndex];
+                gamePadState = input.CurrentGamePadStates[playerIndex];
+
+                // The game pauses either if the user presses the pause button, or if
+                // they unplug the active gamepad. This requires us to keep track of
+                // whether a gamepad was ever plugged in, because we don't want to pause
+                // on PC if they are playing with a keyboard and have no gamepad at all!
+                bool gamePadDisconnected = !gamePadState.IsConnected &&
+                                           input.GamePadWasConnected[playerIndex];
+
+                if (input.IsPauseGame(ControllingPlayer) || gamePadDisconnected)
+                {
+                    ScreenManager.AddScreen(new PauseMenuScreen(), ControllingPlayer);
+                }
+                else
+                {
+
+                }
+            }
+
+            Texture2D UN, Virus, Ninja, Ninja1, Money, Comic;
+            /// <summary>
+            /// Draws the gameplay screen.
+            /// </summary>
+            public override void Draw(GameTime gameTime)
+            {
+                // This game has a blue background. Why? Because!
+                ScreenManager.GraphicsDevice.Clear(Color.Black);
+                Game1.spriteBatch.Begin();
+                Game1.spriteBatch.Draw(text, new Rectangle(0, 0,750,500), Color.White);
+                Game1.spriteBatch.End();
+
+            }
+            #endregion
+        }
+    }
---
+++ trunk/SpaceHaste/SpaceHaste/SpaceHaste/Screens/MainMenuScreen.cs
@@ -0,0 +1,113 @@
+#region File Description
+//-----------------------------------------------------------------------------
+// MainMenuScreen.cs
+//
+// Microsoft XNA Community Game Platform
+// Copyright (C) Microsoft Corporation. All rights reserved.
+//-----------------------------------------------------------------------------
+#endregion
+
+#region Using Statements
+using Microsoft.Xna.Framework;
+using SpaceHaste;
+
+#endregion
+
+namespace GameStateManagement
+{
+    /// <summary>
+    /// The main menu screen is the first thing displayed when the game starts up.
+    /// </summary>
+    class MainMenuScreen : MenuScreen
+    {
+        #region Initialization
+
+
+        /// <summary>
+        /// Constructor fills in the menu contents.
+        /// </summary>
+        public MainMenuScreen()
+            : base("Main Menu")
+        {
+            // Create our menu entries.
+            //Game1.SoundEngine.TurnSoundOn(ConstantSounds.MenuBackground);
+            MenuEntry playGameMenuEntry = new MenuEntry("Play Game");
+            MenuEntry creditsMenuEntry = new MenuEntry("Credits");
+            MenuEntry controlsMenuEntry = new MenuEntry("Controls");
+            MenuEntry exitMenuEntry = new MenuEntry("Exit");
+
+            // Hook up menu event handlers.
+            playGameMenuEntry.Selected += PlayGameMenuEntrySelected;
+            controlsMenuEntry.Selected += ControlsMenuEntrySelected;
+            creditsMenuEntry.Selected += CreditsMenuEntrySelected;
+            exitMenuEntry.Selected += OnCancel;
+
+            // Add entries to the menu.
+            MenuEntries.Add(playGameMenuEntry);
+            MenuEntries.Add(controlsMenuEntry);
+            MenuEntries.Add(creditsMenuEntry);
+            MenuEntries.Add(exitMenuEntry);
+            //Game1.game.PauseGameComponents();
+        }
+
+
+        #endregion
+
+        #region Handle Input
+
+
+        /// <summary>
+        /// Event handler for when the Play Game menu entry is selected.
+        /// </summary>
+        void PlayGameMenuEntrySelected(object sender, PlayerIndexEventArgs e)
+        {
+           // Game1.SoundEngine.TurnSoundOff(ConstantSounds.MenuBackground);
+            LoadingScreen.Load(ScreenManager, true, e.PlayerIndex,
+                               new GameplayScreen());
+        }
+
+
+        /// <summary>
+        /// Event handler for when the Options menu entry is selected.
+        /// </summary>
+        void ControlsMenuEntrySelected(object sender, PlayerIndexEventArgs e)
+        {
+            //ScreenManager.AddScreen(new ControlsScreen(), e.PlayerIndex);
+        }
+
+        /// <summary>
+        /// Event handler for when the Options menu entry is selected.
+        /// </summary>
+        void CreditsMenuEntrySelected(object sender, PlayerIndexEventArgs e)
+        {
+           // ScreenManager.AddScreen(new CreditsScreen(), e.PlayerIndex);
+        }
+
+        /// <summary>
+        /// When the user cancels the main menu, ask if they want to exit the sample.
+        /// </summary>
+        protected override void OnCancel(PlayerIndex playerIndex)
+        {
+            const string message = "Are you sure you want to exit this sample?";
+
+            MessageBoxScreen confirmExitMessageBox = new MessageBoxScreen(message);
+
+            confirmExitMessageBox.Accepted += ConfirmExitMessageBoxAccepted;
+
+            ScreenManager.AddScreen(confirmExitMessageBox, playerIndex);
+        }
+
+
+        /// <summary>
+        /// Event handler for when the user selects ok on the "are you sure
+        /// you want to exit" message box.
+        /// </summary>
+        void ConfirmExitMessageBoxAccepted(object sender, PlayerIndexEventArgs e)
+        {
+            ScreenManager.Game.Exit();
+        }
+
+
+        #endregion
+    }
+}

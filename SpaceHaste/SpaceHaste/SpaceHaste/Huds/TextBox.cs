edd9750fe383c6f37e981c7f07c23b4f104bdc58
trogs@rocketmail.com <trogs@rocketmail.com@d46a54b8-87dd-8360-0e66-63ad6548c8d3>
2011-11-14 16:31:02 +0000

    Fixed the cutscenes.

--- SpaceHaste/SpaceHaste/SpaceHaste/Huds/TextBox.cs
+++ SpaceHaste/SpaceHaste/SpaceHaste/Huds/TextBox.cs
@@ -5,6 +5,7 @@ using System.Text;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using SpaceHaste.GameMech.CutScenes;
+using SpaceHaste.Graphics;
 
 namespace SpaceHaste.Huds
 {
@@ -12,9 +13,11 @@ namespace SpaceHaste.Huds
     {
         Vector2 TextPosition;
         Texture2D texture1;
+        float ScreenWidth = GraphicsManager.graphics.PreferredBackBufferWidth;
+        float ScreenHeight = GraphicsManager.graphics.PreferredBackBufferHeight;
 
-        public TextBox() { 
-            TextPosition = new Vector2(10, 600);
+        public TextBox() {
+            TextPosition = new Vector2((int)(ScreenWidth / 3) + 10, (int)(4 * (ScreenHeight / 5)));
             texture1 = Hud.Content.Load<Texture2D>("UI_backPane_blue");
 
         }
@@ -26,12 +29,17 @@ namespace SpaceHaste.Huds
             Hud.spriteBatch.Begin();
            
             
-            Hud.spriteBatch.Draw(texture1, new Rectangle(10, 600, 150, 500), Color.White);
+            Hud.spriteBatch.Draw(texture1, new Rectangle((int)(ScreenWidth / 3), (int)(4 * (ScreenHeight / 5)), (int)(ScreenWidth/3), (int)(ScreenHeight/5)), Color.White);
             Hud.spriteBatch.DrawString(Hud.spriteFont, text, TextPosition, Color.Yellow, 0, Vector2.Zero, 1.1f, SpriteEffects.None, 0);
             
             
             Hud.spriteBatch.End();
 
+        }
+
+        public void endBox() {
+            ScreenWidth = 0;
+            ScreenHeight = 0;
         }
 
     }

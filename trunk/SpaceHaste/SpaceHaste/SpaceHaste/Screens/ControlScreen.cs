0d476ef6512a9ac58943acf4cf5bebffdd611ff3
howlingblue@gmail.com <howlingblue@gmail.com@d46a54b8-87dd-8360-0e66-63ad6548c8d3>
2011-12-06 05:55:41 +0000

    Added controls screen with keyboard image. Gamepad image will come tomorrow, I promise!

---
+++ trunk/SpaceHaste/SpaceHaste/SpaceHaste/Screens/ControlScreen.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+
+namespace GameStateManagement
+{
+    class ControlScreen : MenuScreen
+    {
+        bool keyboardOn;
+        ContentManager content;
+        Texture2D GPPic;
+        Texture2D keyPic;
+
+        public ControlScreen()
+            : base("Controls")
+        {
+            keyboardOn = true;
+
+            MenuEntry keyboard = new MenuEntry("Keyboard");
+            keyboard.Selected += keyboardSelected;
+
+            MenuEntries.Add(keyboard);
+        }
+
+        public override void LoadContent()
+        {
+            if (content == null)
+                content = new ContentManager(ScreenManager.Game.Services, "Content");
+            keyPic = content.Load<Texture2D>("keyMaps");
+        }
+
+        void keyboardSelected(object sender, PlayerIndexEventArgs e)
+        {
+            keyboardOn = false;
+            MenuEntries.Clear();
+            MenuEntry gamepad = new MenuEntry("Gamepad");
+            gamepad.Selected += gamepadSelected;
+            MenuEntries.Add(gamepad);
+
+        }
+
+        void gamepadSelected(object sender, PlayerIndexEventArgs e)
+        {
+            keyboardOn = true;
+            MenuEntries.Clear();
+            MenuEntry keyboard = new MenuEntry("Keyboard");
+            keyboard.Selected += keyboardSelected;
+            MenuEntries.Add(keyboard);
+        }
+
+        protected override void OnCancel(PlayerIndex playerIndex)
+        {
+            base.OnCancel(playerIndex);
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            SpriteBatch spriteBatch = ScreenManager.SpriteBatch;
+            Viewport viewport = ScreenManager.GraphicsDevice.Viewport;
+            Rectangle size; 
+
+            spriteBatch.Begin();
+
+            if (keyboardOn)
+            {
+                size = new Rectangle((viewport.Width / 2) - (keyPic.Width / 2), (viewport.Height / 2) - (keyPic.Height / 2), 
+                                        keyPic.Width, keyPic.Height);
+                spriteBatch.Draw(keyPic, size, new Color(TransitionAlpha, TransitionAlpha, TransitionAlpha));
+            }
+
+            spriteBatch.End();
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ trunk/SpaceHaste/SpaceHaste/SpaceHaste/Screens/CreditsScreen.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using SpaceHaste;
+
+namespace GameStateManagement
+{
+    class CreditsScreen : MenuScreen
+    {
+        List<String> creditRoll;
+
+        public CreditsScreen()
+            : base("Credits")
+        {
+            MenuEntry backEntry = new MenuEntry("Back to Title");
+            backEntry.Selected += BackSelected;
+            MenuEntries.Add(backEntry);
+
+            //List of credits
+            creditRoll = new List<String>();
+            creditRoll.Add("Engine Programming: Thomas Robbins");
+            creditRoll.Add("AI and Controls Programming: Vincent Kocks");
+            creditRoll.Add("Story and Gameplay Programming: Matthew Leary");
+            creditRoll.Add("Music and UI Programming: Trevor Gray");
+            creditRoll.Add("Modeling and Art: Jorge Cereijo");
+        }
+
+        void BackSelected(object sender, PlayerIndexEventArgs e)
+        {
+            OnCancel(e.PlayerIndex);
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            SpriteBatch spriteBatch = ScreenManager.SpriteBatch;
+            SpriteFont font = ScreenManager.Font;
+
+            // Add variables for centering credits.
+            Viewport viewport = ScreenManager.GraphicsDevice.Viewport;
+            Vector2 viewportSize = new Vector2(viewport.Width, viewport.Height);
+            Vector2 textSize = new Vector2(0, 0);
+            Vector2 textPosition = (viewportSize - textSize) / 2;
+            int startY = 200;
+
+            //Print the credits
+            spriteBatch.Begin();
+            foreach (String person in creditRoll)
+            {
+                textSize = font.MeasureString(person);
+                textPosition = (viewportSize - textSize) / 2;
+                textPosition.Y = startY;
+                spriteBatch.DrawString(font, person, textPosition, Color.White);
+                startY += 40;
+            }
+            spriteBatch.End();
+            base.Draw(gameTime);
+        }
+    }
+}
